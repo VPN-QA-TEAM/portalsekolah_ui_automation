4edd0d21f1bb0d2ebb4a6986f1692e88b31a7abe
Miftah <miftah.ramadhan@noble-software.com>
2023-11-20 09:35:59 +0700

    file AssessmentKmPage.py

---
+++ Pages/AssessmentKmPage.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+from Pages.BaseMethod import MyGenericMethods
+
+class Login(MyGenericMethods):
+
+    """Locators Assessment page"""
+    LOC_Dropdown_Course = ""
+
+    """Constructor of the page class"""
+    def __init__(self, driver):
+        super().__init__(driver)
+        pass
+
